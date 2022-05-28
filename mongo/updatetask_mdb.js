aa7df08e9294007789d8bacdf9a3481a59e48542
eliashz <eliashz@gmail.com>
2022-05-28 19:42:19 +0200

    updatetask WIP

--- mongo/updatetask_mdb.js
+++ mongo/updatetask_mdb.js
@@ -13,27 +13,35 @@ main().catch(err => console.log(err));
 //TO DO the config of the path should probably be parametres???
 async function main() {
     await mongoose.connect('mongodb://localhost:27017/todoDB');
+/*     let toDo1 = new ToDo({id: 5, text: 'Something to do.', userName: 'Elías'})
+    let toDo2 = new ToDo({id: 6, text: 'Something to do 2.', userName: 'Huerta'})
+    toDo1.save()
+    toDo2.save() */
 }
 
-let toDo1 = new ToDo({id: 1, text: 'Something to do.', userName: 'Elías'})
-let toDo2 = new ToDo({id: 2, text: 'Something to do 2.', userName: 'Huerta'})
-toDo1.save()
-toDo2.save()
-console.log(toDo2);
+
+//console.log(toDo2);
 //LIST OF QUERIES:
 //https://mongoosejs.com/docs/api/query.html
 
                   
 const updateTask = async (id, newState) => {
-    const filter = { id: id }
-    const update = { state: newState }
-    console.log(newState,);
-    let doc = await ToDo.findOneAndUpdate(filter, update);
+    const searchID = await ToDo.find({id: id})
+    if (!searchID) {
+        console.log(`ID ${id} doesn't exist!`);
+    } else {
+        const filter = { id: id }
+        const update = { state: newState }
+        let doc = await ToDo.findOneAndUpdate(filter, update);
+    }
+    
+    //console.log(toDo2);
+
 
 
 }         
 
-updateTask(1, 'executing')
+updateTask(2, 'executing')
 
 const showTaskState = async () => {
     console.log("showtaskstate");
