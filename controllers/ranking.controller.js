14fac484100fa62363468417f3bf52a246b82e32
ecorbero <ecorbero@gmail.com>
2022-06-27 13:21:35 +0200

    push array

--- controllers/ranking.controller.js
+++ controllers/ranking.controller.js
@@ -5,7 +5,9 @@ exports.rankingGames = async (req, res) => {
     try {
       const result  = await Players.find({},{username:1,average:1}).sort({ average: -1});
   
-      console.log(result);
+      const result2 = await Players.aggregate([{$group: {_id:null, avg_val:{$avg:"$average"}}}]);
+
+      result.push(result2);
   
       res.status(200).json(result);
   
